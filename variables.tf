a3c64b911f8721afbdd1d5f2a6f3aabb291d8a9d
Prasath Sivasubramaniyan <tsprasath91@gmail.com>
2017-09-03 10:02:33 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
 variable "region" { default = "us-west-2" }
 variable "instance_type" { default = "t2.micro" }
-#variable "key_name" { default = "Terraform" }
+variable "key_name" { default = "prasath" }
