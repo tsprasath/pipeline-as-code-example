06cca7182a766c90a120fc681f16aa1c7299e291
Prasath Sivasubramaniyan <tsprasath91@gmail.com>
2017-09-03 10:04:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_launch_configuration" "test-lc" {
   name_prefix = "test-lc-"
   image_id = "${data.aws_ami.ubuntu.id}"
   instance_type = "${var.instance_type}"
- 
+  key_name = "${var.key_name}"
   security_groups = [ "${aws_security_group.instance.id}" ]
 
   lifecycle {
