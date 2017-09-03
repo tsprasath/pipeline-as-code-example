5870f510f20d4b8f31aba5db2fcd2a827539b7a7
Prasath Sivasubramaniyan <tsprasath91@gmail.com>
2017-09-03 09:51:51 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
 variable "region" { default = "us-west-2" }
 variable "instance_type" { default = "t2.micro" }
-variable "key_name" { default = "ubuntu" }
+#variable "key_name" { default = "Terraform" }
